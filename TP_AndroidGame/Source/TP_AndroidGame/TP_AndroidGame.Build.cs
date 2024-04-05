bd44eb777b74fa7a937106eec1be8f3d4842eb4b
Fabio Pittaccio <pittacciofabio@gmail.com>
2024-04-05 17:01:58 +0200

    edit .Build.cs

--- TP_AndroidGame/Source/TP_AndroidGame/TP_AndroidGame.Build.cs
+++ TP_AndroidGame/Source/TP_AndroidGame/TP_AndroidGame.Build.cs
@@ -9,6 +9,6 @@ public class TP_AndroidGame : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore"});
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 	}
 }
